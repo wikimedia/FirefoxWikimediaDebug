848239df87e9beb031add598074fcc2240cab259
Bryan Davis <bd808@wikimedia.org>
2016-03-15 13:20:41 -0600

    Add support for "profile" and "readonly" options
    Add support for passing the "profile" and "readonly" options to
    Wikimedia servers.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Wikimedia Debug Header Firefox Add-on
 
 This Firefox addon injects a `X-Wikimedia-Debug` header into HTTP requests to
 Wikimedia Foundation projects. Requests bearing this header are not cached and
-are all handled by a single machine.
+are routed to machines dedicated to debugging where additional logging and/or
+profiling is available. See [wikitech] for additional documentation.
 
 There is also a [Chrome version].
 
@@ -24,6 +25,7 @@ Wikimedia Debug Header Firefox Add-on is licensed under the Apache 2.0
 license. See the `LICENSE` file for more details.
 
 
+[wikitech]: https://wikitech.wikimedia.org/wiki/X-Wikimedia-Debug
 [Add-on SDK]: https://developer.mozilla.org/en-US/Add-ons/SDK
 [addons.mozilla.org]: https://addons.mozilla.org/en-US/firefox/addon/wikimedia-debug-header/
 [Chrome version]: https://github.com/wikimedia/ChromeWikimediaDebug
--- data/prefs.js
+++ data/prefs.js
@@ -1,12 +1,24 @@
 var eBackend = document.getElementById("backend"),
-    eWrapper = document.getElementById("wrapper");
+    eProfile = document.getElementById("profile"),
+    eReadonly = document.getElementById("readonly"),
+    eContainer = document.getElementById("container");
 
 eBackend.addEventListener("input", function (e) {
     self.port.emit("change", "backend", eBackend.value);
 }, false);
 
-self.port.on("show", function (backend) {
+eProfile.addEventListener("change", function (e) {
+    self.port.emit("change", "profile", eProfile.checked);
+}, false);
+
+eReadonly.addEventListener("change", function (e) {
+    self.port.emit("change", "readonly", eReadonly.checked);
+}, false);
+
+self.port.on("show", function (backend, profile, readonly) {
     eBackend.value = backend;
-    self.port.emit("resize", eWrapper.scrollWidth, eWrapper.scrollHeight);
+    eProfile.checked = profile;
+    eReadonly.checked = readonly;
+    self.port.emit("resize", eContainer.scrollWidth, eContainer.scrollHeight);
 });
 
